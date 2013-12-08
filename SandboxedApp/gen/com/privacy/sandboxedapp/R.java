a24398027f1ebe439f6c05f30951030fbba8579d
Matthias Bahlke <mrfuzzemz+github@gmail.com>
2013-12-08 11:28:03 -0500

    Added more request info

--- SandboxedApp/gen/com/privacy/sandboxedapp/R.java
+++ SandboxedApp/gen/com/privacy/sandboxedapp/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080004;
+        public static final int buttonIMEI=0x7f080003;
         public static final int buttonLoc=0x7f080001;
+        public static final int buttonProf=0x7f080002;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
@@ -37,8 +39,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int requestIMEIButton=0x7f050005;
         public static final int requestLocationButton=0x7f050003;
+        public static final int requestProfileButton=0x7f050004;
+        public static final int title=0x7f050002;
     }
     public static final class style {
         /** 
