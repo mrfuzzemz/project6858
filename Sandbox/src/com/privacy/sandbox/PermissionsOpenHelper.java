95d6bf7b436591078fdd67b4aa96fe1b8fd0d312
finche <finche@mit.edu>
2013-12-07 20:12:43 -0500

    should be a working db interaction class

--- Sandbox/src/com/privacy/sandbox/PermissionsOpenHelper.java
+++ Sandbox/src/com/privacy/sandbox/PermissionsOpenHelper.java
@@ -1,4 +1,5 @@
 package com.privacy.sandbox;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,17 +9,16 @@ public class PermissionsOpenHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 2;
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_NAME = "comment";
+    public static final String COLUMN_NAME = "name";
     public static final String COLUMN_PROFILE = "read_profile";
-    public static final String COLUMN_PROFILE_VALUE = "read_profile_value";
     private static final String DATABASE_NAME = "permissions.db";
-    private static final String PERMISSIONS_TABLE_NAME = "permissions";
+    public static final String PERMISSIONS_TABLE_NAME = "permissions";
 
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
         + PERMISSIONS_TABLE_NAME + "(" + COLUMN_ID
         + " integer primary key autoincrement, " + COLUMN_NAME 
-        + " text not null, " + COLUMN_PROFILE + " text not null, " + COLUMN_PROFILE_VALUE + " text not null);";
+        + " text not null, " + COLUMN_PROFILE + " text not null);";
 
     public PermissionsOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
