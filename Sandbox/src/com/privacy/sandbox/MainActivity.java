b129b08d90430d3f20ae3cfd86dc107787e4e3f8
Matthias Bahlke <mrfuzzemz+github@gmail.com>
2013-12-08 19:19:22 -0500

    Added Sandbox acquiring phone IMEI, owner name, and wireless carrier.

--- Sandbox/src/com/privacy/sandbox/MainActivity.java
+++ Sandbox/src/com/privacy/sandbox/MainActivity.java
@@ -1,8 +1,12 @@
 package com.privacy.sandbox;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.widget.TextView;
 
@@ -15,7 +19,27 @@ public class MainActivity extends Activity {
         
 		TextView requestLabel =(TextView)findViewById(R.id.textView1);
 		// requestLabel.setText("Profile");
+		
+		// Get the current carrier
+		TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+		String carrierName = telephonyManager.getNetworkOperatorName();
         
+		// requestLabel.setText(carrierName);
+        
+		// Get IMEI
+		String phoneIMEI = telephonyManager.getDeviceId();
+		// requestLabel.setText(phoneIMEI);
+		
+		// Get the phone owner's name (be sure to set this up on the phone!!!)
+		Cursor c = this.getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, null, null);
+		c.moveToFirst();
+		String userName = c.getString(c.getColumnIndex("DISPLAY_NAME"));
+		c.close();
+		requestLabel.setText(userName);
+
+
+		
+		
     }
 
 
