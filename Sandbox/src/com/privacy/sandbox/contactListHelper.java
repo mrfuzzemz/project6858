420c4a3e645c8fc21a7b707fe962e9759507d741
whuffman <whuffman91@gmail.com>
2013-12-10 16:29:03 -0500

    Added ability to get contact phone information.

--- Sandbox/src/com/privacy/sandbox/contactListHelper.java
+++ Sandbox/src/com/privacy/sandbox/contactListHelper.java
@@ -21,9 +21,17 @@ public class contactListHelper {
 		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 		
 		while(cur.moveToNext()) {
-			   int nameFieldColumnIndex = cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-			   String contact = cur.getString(nameFieldColumnIndex);
-			   contactList.add(contact+":"+"phone");
+			   String contact = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+			   String contactNumber = "phone_not_available";
+		       String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+		       if(Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+		    	   Cursor pcur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",new String[]{ id }, null);
+		    	   if(pcur.moveToFirst()) {
+		    		   contactNumber = pcur.getString(pcur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+		    	   }
+		    	   pcur.close();
+		       }
+			   contactList.add(contact+":"+contactNumber);
 		}
 			
 		cur.close();
