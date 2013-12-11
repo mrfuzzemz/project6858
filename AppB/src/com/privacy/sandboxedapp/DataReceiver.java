16ccf08ee1ad022d2b5a66fa9bed08d84ac1f057
finche <finche@mit.edu>
2013-12-10 20:11:51 -0500

    added a copy of Sandboxed (AppB) to the repo (for demo of apps w/ different permission settings)

---
+++ AppB/src/com/privacy/sandboxedapp/DataReceiver.java
@@ -0,0 +1,17 @@
+package com.privacy.sandboxedapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class DataReceiver extends BroadcastReceiver {
+
+		 @Override
+		 public void onReceive(Context arg0, Intent arg1) {
+			String data = arg1.getExtras().getString("data");
+			
+			Toast.makeText(arg0, "SandboxedApp received " + data, Toast.LENGTH_LONG).show();
+		 }
+
+}
